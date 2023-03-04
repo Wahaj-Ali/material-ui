d953c603b516b926f5fb0048b298bdc60bfcd617
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-03-04 18:15:42 +0500

    Get know-how of Material UI
    In this commit
    - Play with different Material UI components  to get an understanding.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import { Button } from "@mui/material";
+import { Button, styled, Typography } from "@mui/material";
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
+
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+        color:"#888",
+        margin:5,
+        "&:hover":{
+          backgroundColor: "lightblue"
+        },
+        "&:disabled": {
+          backgroundColor: "grey",
+          color: "white"
+        },
+  });
   return (
     <div>
-     Hello!
+      <Button variant="text">Text</Button>
+      <Button variant="contained" color="primary" startIcon={<SettingsIcon />}>Settings</Button>
+      <Button variant="contained" color="secondary" startIcon={<AddIcon />}>Add new post</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Typography variant="h1">
+        h1 Heading
+      </Typography>;
+      <BlueButton>My Button</BlueButton>
     </div>
   );
 }
